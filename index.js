987053591078a097e087d36346657f64c7421425
Faiz Naeem <81919141+FaizNaeem@users.noreply.github.com>
2023-10-10 18:18:23 +0500

    Put and delete

--- index.js
+++ index.js
@@ -1,10 +1,69 @@
 const express = require("express")
-const app = express()
-
-app.get('/',(req , res)=>{
-res.send('server Ready ')
+var app = express()
+const user = require("./user");
+const morgan = require("morgan");
+app.use(morgan("tiny"))
+app.use(express.json())
+app.use('/user', user)
+const middelever=(req ,res , next)=>{
+console.log("Middlewere console");
+next()
+}
+app.use(middelever)
+app.get("/", (req ,res)=>{
+    res.send("Server ready to Go hello")
 })
-let portel = 3000
-app.listen(portel,()=>{
+// get user id using params 
+// app.get('/user/:id', (req, res) => {
+//     console.log('console.log-->', req.params.id)
+//     console.log('console.log-->', req.query)
+//     res.send({ 
+//         status: 200,
+//         msg: "User id is " + req.params.id
+//     })
+// })
+app.get("/about", (req ,res)=>{
+    res.send({
+        status :200,
+        massage :"About working"
+    })
+})
+
+
+
+
+
+
+
+
+
+
+app.listen(3000,()=>{
     console.log("server ready" + 3000);
 })
+// some step use node js 
+// / asynchronous / non-blocking 
+// synchronous  / blocking 
+// Request Fucntion / Get / Put / Patch / Post / Delete
+//command 
+// step 01 --> : npm init
+// step 02 --> : npm i express
+// step 03 -->: npm i nodemon -g
+// step 04 -->: Run Terminal / node index.js
+// step 05 -->: Run Terminal / nodemon index.js
+// step 06 --> : Install Thunder Client Extention And Go to Thander Client and click new Request and paste url 
+
+// Javascript Client Side rendering
+// Node.js Server Side rendering
+
+// user ne request kya router ke pass aya phir request gae controller ke pass controller ne bheja service ke pass phir service ne response dya phir response gya controller ke pass controller ne response bhej dya user ko
+
+// request se service ke pass arha he service se response ja rha he
+//service /  route / controller
+
+
+
+//Get / Lene ke lye
+//Put / Update Krne ke lye
+//Post / Post krne ke lye
+//Delete / Delete krne lye
---
+++ user.js
@@ -0,0 +1,72 @@
+const app = require("express")
+const router = app.Router()
+const user = [
+    {
+    Name:"faiz",
+    id:1
+},
+{
+    Name:"Junaid",
+    id:2
+},
+{
+    Name:"Atta",
+    id:3
+},
+{
+    Name:"Bilal",
+    id:4
+},
+]
+router.get('/',(req, res)=>{
+res.status(200).send({
+    status:200,
+    massage:"All users",
+    Users:user
+})
+// console.log(user);
+})
+router.get('/:id',(req, res)=>{
+    let user1 = user.find(e=>e.id==req.params.id)
+    if(user1){
+        res.status(200).send({
+            user:user1,
+            Meassage :"User Fonuded"
+
+        })
+    }
+    else{
+        res.status(500).send(
+            {
+                status:500,
+                Error:true,
+                massage:"User Not Found"
+                
+        }
+        )
+    }
+})
+router.post('/', (req, res) => {
+    console.log(req.body)
+    user.push({Name :req.body.Name , id : user.length  })
+    res.status(200).send({ status: 200, user : {Name :req.body.Name , id : user.length + 1 } })
+
+})
+router.delete('/:id', (req, res) => {
+    console.log(req.body)
+    user.splice(req.params.id-1 ,1)
+    res.status(200).send({ status: 200, user : user })
+
+})
+router.put('/:id', (req, res) => {
+    
+    if(user[req.params.id-1]){
+        user[req.params.id-1].Name= 'faizu'
+        res.status(200).send({ status: 200, user : user[req.params.id-1].Name })
+    }
+else{
+    res.status(400).send({ status: 400, Massage : "user not found"})
+    
+}
+})
+module.exports= router
